875bfd2175aa6b49a0bde3e61e97be9ddb0b5f50
hwangJi-dev <melon2228@swu.ac.kr>
2021-09-22 13:57:30 +0900

    [Chore] UIColor+Extension에 컬러 추가

---
+++ Starbucks_Clone/Starbucks_Clone/Global/Extension/UIColor+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extension.swift
+//  Starbucks_Clone
+//
+//  Created by 황지은 on 2021/09/22.
+//
+
+import UIKit
+extension UIColor {
+
+  @nonobjc class var seaweedGreen: UIColor {
+    return UIColor(red: 46.0 / 255.0, green: 153.0 / 255.0, blue: 95.0 / 255.0, alpha: 1.0)
+  }
+}
